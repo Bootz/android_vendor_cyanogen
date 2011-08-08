ef899abf18d448f27afefae342d2b7af578ec012
Justin E. Goodell <jegoodell@gmail.com>
2011-08-08 12:54:00 -0400

    add venus2 to vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,6 +40,7 @@ add_lunch_combo cyanogen_speedy-eng
 add_lunch_combo cyanogen_supersonic-eng
 add_lunch_combo cyanogen_u8220-eng
 add_lunch_combo cyanogen_vega-eng
+add_lunch_combo cyanogen_venus2-eng
 add_lunch_combo cyanogen_vibrantmtd-eng
 add_lunch_combo cyanogen_vision-eng
 add_lunch_combo cyanogen_vivo-eng
